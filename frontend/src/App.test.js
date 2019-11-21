c4e74330e0279a5efb3183c2c3f8bb04300ee89c
Manuel Baronetti <manuelbaronetti@gmail.com>
2019-11-21 19:32:51 -0300

    Initial test rtl

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {render, fireEvent, cleanup} from '@testing-library/react';
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
 import App from './App';
 
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
 it('renders without crashing', () => {
+  const store = mockStore({})
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
